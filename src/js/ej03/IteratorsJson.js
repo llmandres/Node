95634981df6d0c1dd2114fc1888aaceeea248d55
Yolmi <yolminey@gmail.com>
2025-01-30 19:02:52 +0100

    Correcciones

--- src/js/ej03/IteratorsJson.js
+++ src/js/ej03/IteratorsJson.js
@@ -29,7 +29,9 @@ console.log(data.abilities.map((elem) => 'Ability: ' + elem.ability.name))
 // Filtra los movimientos (moves) que se pueden aprender en el nivel inicial (level_learned_at: 0).
 // Devuelve un array con los nombres de estos movimientos. (Posible requisito: Filter, Some, Map)
 
-// console.log(data.moves.filter((elem) => elem.version_group_details))
+console.log(data.moves[0].version_group_details.some(elem => elem.level_learned_at === 0)) // Verifica si hay ALGUN movimiento que se aprenda a nivel 0
+const moveFilter = data.moves.filter(move => move.version_group_details.some(elem => elem.level_learned_at === 0))
+console.log(moveFilter.map(move => move.move.name))
 
 // Encuentra la habilidad que tenga is_hidden igual a true y devuelve su nombre.
 // Si no existe, devuelve un mensaje indicando que no hay habilidades ocultas.
@@ -39,9 +41,15 @@ if (trueAbilities) {
 } else {
   console.log('No hay habilidades ocultas')
 }
+// Otra manera
+// SE USA MAP PARA PASAR DE UN ARRAY A OTRO ARRAY
+const abilitiesFilter = data.abilities.filter(ability => ability.is_hidden === true) // Muestra el objeto que tiene true en is hidden
 
+console.log(abilitiesFilter.length ? abilitiesFilter.map(ability => ability.ability.name) : 'No hay habilidades que no sean ocultas')
 // Calcula el número total de juegos (game_indices) en los que aparece este Pokémon.
 // (Posible requisito: Reduce)
+const numeroJuegos = data.game_indices.length
+console.log('Juegos en los que aparece : ' + numeroJuegos + '\n' + data.game_indices.map(juego => 'Juego : ' + juego.version.name + '\n'))
 
 // Recorre el array de held_items y muestra por consola el nombre de cada ítem
 // junto con las versiones en las que está disponible. El output tiene que tener el
@@ -51,6 +59,10 @@ if (trueAbilities) {
 // Verifica si todas las habilidades (abilities) no son ocultas (is_hidden).
 // Además, verifica si al menos un movimiento tiene el método de aprendizaje "machine".
 // (Posible requisito: every, some)
+console.log(data.abilities.every(abilities => abilities.is_hidden === true))
+console.log(data.moves.every(move =>
+  move.version_group_details?.some(detail => detail.move_learn_method.name === 'machine')
+))
 
 // Convierte el array de habilidades (abilities) en un objeto donde las claves
 // sean los nombres de las habilidades y los valores indiquen si son ocultas (is_hidden).
